52e3a06dba0bf69e1ee9f4898c72b5fe517e62e5
ilsenatorov <il.senatorov@gmail.com>
2019-11-26 18:11:09 +0100

    added remove node and edge

--- graph.cpp
+++ graph.cpp
@@ -1,20 +1,28 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include <unordered_map>
+#include <map>
 #include <string>
-#include <unordered_set>
+#include <set>
 
 using namespace std;
 
 struct Node {
     string id;
-    unordered_set<string> neighbours;
+    set<string> neighbours;
     Node(){};
     Node(const string &id) : id(id) {}
     void add_edge(const string &neighbour) {
         neighbours.insert(neighbour);
     }
+
+    void remove_edge(const string &node) {
+        neighbours.erase(node);
+    }
+
     string get_neighbours() const {
+        if (neighbours.empty()) {
+            return "[]";
+        }
         string res("[");
         string comma(", ");
         for (auto i : neighbours) {
@@ -31,14 +39,26 @@ struct Node {
 
 
 struct Graph {
-    unordered_map<string, Node> nodes;
+    map<string, Node> nodes;
     Graph() {};
     void add_node(const string &node) {
-        nodes.insert(make_pair(node, Node(node)));;
+        nodes[node] = Node(node);
     }
     void add_edge(const string &node1, const string &node2) {
         nodes[node1].add_edge(node2);
         nodes[node2].add_edge(node1);
+    }
+
+    void remove_node(const string &node) {
+        for (auto i : nodes[node].neighbours) {
+            remove_edge(node, i);
+        }
+        nodes.erase(node);
+    }
+
+    void remove_edge(const string &node1, const string &node2) {
+        nodes[node1].remove_edge(node2);
+        nodes[node2].remove_edge(node1);
     }
 
     string __str__ () const {
@@ -57,19 +77,19 @@ struct Graph {
 
 
 namespace py = pybind11;
-
 PYBIND11_MODULE(node, m) {
     py::class_<Node>(m, "Node")
         .def(py::init<const string &>())
-        .def("__str__", &Node::__str__)
-        .def("add_edge", &Node::add_edge);
+        .def("add_edge", &Node::add_edge)
+        .def("degree", &Node::degree);
 }
 
 PYBIND11_MODULE(graph, m) {
     py::class_<Graph>(m, "Graph")
         .def(py::init<>())
         .def("add_node", &Graph::add_node)
+        .def("remove_node", &Graph::remove_node)
+        .def("remove_edge", &Graph::remove_edge)
         .def("__str__", &Graph::__str__)
-
         .def("add_edge", &Graph::add_edge);
 }
---
+++ test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from graph import Graph
+
+G = Graph()
+G.add_node("first")
+G.add_node("second")
+G.add_node("third")
+G.add_node("fourth")
+
+G.add_edge("first", "second")
+G.add_edge("first", "third")
+G.add_edge("third", "fourth")
+print(G)
+G.remove_edge("first", "second")
+print(G)
